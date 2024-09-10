d647f866b795d1f7bcc449b87941e25c7ae01803
nitecse <hitman12457868@gmail.com>
2024-09-10 14:47:19 +0530

    all done 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [counterVal, setCounterVal] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <h1>Counter App</h1>
+      <h5>Count: {counterVal}</h5>
+      <button onClick={() => { setCounterVal(counterVal + 1); }}>Increment</button>
+      <button onClick={() => { setCounterVal(counterVal - 1); }}>Decrement</button>
     </div>
   );
 }
